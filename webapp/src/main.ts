3fe7d1e60c9984ebe2350f5ca310b0e440cbdd3f
Vinicius Amaral <vinicius.santosama@gmail.com>
2025-10-07 02:26:11 -0300

    More base Angular configurations

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -2,5 +2,36 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import {
+  Chart,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  ArcElement,
+  ScatterController,
+  BarController,
+  PieController
+} from 'chart.js';
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  ArcElement,
+  ScatterController,
+  BarController,
+  PieController
+);
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
